2b6799dcc25298dd29bf2bd8e9b24c4643f519a4
Yassinetebib147 <yassine.tebib.algo@gmail.com>
2021-03-29 11:58:55 +0200

    First Commit

---
+++ Assets/Scripts/EyeGameOver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeGameOver : MonoBehaviour
+{
+   
+    void OnCollisionEnter(Collision newCollision)
+    {
+        if (newCollision.gameObject.tag == "Player")
+        {
+            // if game manager exists, make adjustments based on target properties
+            if (GameManager.gm)
+            {
+                GameManager.gm.gameIsOver = true ;
+            }
+
+        }
+        // destroy the projectile
+        Destroy(newCollision.gameObject);
+
+    }
+}
